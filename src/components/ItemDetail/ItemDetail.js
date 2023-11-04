93f7df49cea4052e19347fd0c771175a58f519b7
Matimrs <mati.morales.tepp@gmail.com>
2023-11-04 14:26:47 -0300

    Implementacion de Agregar al Carrito

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
 import Count from "../Count/Count";
 import "./ItemDetail.css";
+import { useState } from "react";
 
 const ItemDetail = ({id,name,price,img,category,stock,description}) => {
+
+    const [quantityAdded, setQuantityAdded] = useState(0);
+
+    const handleOnAdd = (quantity)=>{
+        setQuantityAdded(quantity);
+    }
+    const handleOnCancel = ()=>{
+        setQuantityAdded(0);
+    }
+
 return(
     <article className="itemDetail box">
         <header className="centrado">
@@ -16,7 +28,18 @@ return(
             <p className="subtitle is-6">Descripcion: {description}</p>
         </section>
         <footer className="centrado">
-            <Count initial={1} stock={stock} onAdd={(q)=>{ console.log("Cantidad agregada" + q) }} />
+            {
+                quantityAdded > 0 ?
+                (<div>
+                    <Link to='/cart'>
+                        <button className="button is-link">Confirmar</button>
+                    </Link>
+                    <button className="button is-link" onClick={handleOnCancel}>Cancelar</button>
+                </div>
+                )
+                :
+                (<Count initial={1} stock={stock} onAdd={handleOnAdd} />)
+            }
         </footer>
     </article>
 )
